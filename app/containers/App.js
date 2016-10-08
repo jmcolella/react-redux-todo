1eebd89b70fcbf96b60c01ce25f6ab58eb7530f3
John Colella <colella.john@gmail.com>
2016-10-08 15:02:33 -0400

    complete bare skeleton for react/webpack app

---
+++ app/containers/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react';
+
+const App = () => (
+  <div>
+    Hello world!
+  </div>
+);
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello world!
+//       </div>
+//     )
+//   }
+// };
+
+export { App };
