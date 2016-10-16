5d93be9e803a58b0051ea0ff07b399ac3ff4fe41
jmcolella <colella.john@gmail.com>
2016-10-16 13:23:08 -0400

    work through refactoring es6 syntax

--- app/components/AddTodo.js
+++ app/components/AddTodo.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { dispatch } from 'redux';
 import { addTodo } from '../actions/todo_actions';
 
-const AddTodo = ( { store } ) => (
-  <div>
-    <input ref={ node => { input = node } } type="text" placeholder="add new To Do item" />
-    <button onClick={ dispatch(addTodo( input.value )) }>add item</button>
-  </div>
-);
+const AddTodo = ( { store } ) => {
+  let input;
 
-export { AddTodo };
+  return (
+    <div>
+      <input ref={ node => { input = node } } type="text" placeholder="add new To Do item" />
+      <button onClick={ dispatch(addTodo( input.value )) }>add item</button>
+    </div>
+  )
+};
+
+export default { AddTodo };
