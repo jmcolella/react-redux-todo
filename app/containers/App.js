79d1310eb10a0c3d9489c9b307f1dd03fd22a855
John Colella <colella.john@gmail.com>
2016-10-29 15:29:52 -0400

    add css-loader and work on figuring out how it works

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Component } from 'react';
 import AddTodo from '../components/AddTodo';
 import TodoList from '../containers/TodoList';
 import Footer from '../components/Footer';
+import styles from './application.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <AddTodo />
 
       <TodoList />
