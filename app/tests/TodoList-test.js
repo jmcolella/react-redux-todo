ab3ea21cd6e32d4b9ed99d94eea44c009c2f6df6
John Colella <colella.john@gmail.com>
2016-11-14 23:22:46 -0500

    add test for rendering when a todo is part of the store context

--- app/tests/TodoList-test.js
+++ app/tests/TodoList-test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, dispatch } from 'redux';
 import { todoApp } from '../reducers/todo_reducers';
 import { addTodo } from '../actions/todo_actions';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import TodoList from '../containers/TodoList';
 import Todo from '../components/Todo';
@@ -12,7 +12,7 @@ describe ( 'TodoList Container', () => {
 
   beforeEach( () => {
     store = createStore( todoApp );
-    todoList = shallow( <TodoList />, {
+    todoList = mount( <TodoList />, {
       context: { store }
     });
   });
@@ -21,10 +21,10 @@ describe ( 'TodoList Container', () => {
     expect( todoList.find( '.todo-list-container' ) ).to.have.length( 1 );
   });
 
-  // it ( 'should render Todo component if todos present', () => {
-  //   todoList.context().store.dispatch( addTodo( "New Todo" ) );
-  //   todoList.update();
-  //   expect( todoList.find( Todo ) ).to.have.length( 1 );
-  // });
+  it ( 'should render Todo component if todos present', () => {
+    todoList.context().store.dispatch( addTodo( "New Todo" ) );
+    todoList.update();
+    expect( todoList.find( Todo ) ).to.have.length( 1 );
+  });
 
 });
